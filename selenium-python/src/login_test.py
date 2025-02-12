eba4ff58adb4d86edcce0f5f8b49696564da5187
unknown <Minskoleopc>
2025-02-12 19:55:20 +0530

    added

---
+++ selenium-python/src/login_test.py
@@ -0,0 +1,11 @@
+import math
+def test_sqrt2():
+    num = 25
+    assert 25 == num
+
+def testsquare2():
+    num = 7
+    assert True == True
+
+def testEqual2():
+    assert 10 == 10
---
+++ selenium-python/src/test_math.py
@@ -0,0 +1,12 @@
+import math
+def test_sqrt():
+    num = 25
+    assert 25 == num
+
+def testsquare():
+    num = 7
+    assert math.sqrt(7) == 4
+
+def testEqual():
+    assert 10 == 11
+
