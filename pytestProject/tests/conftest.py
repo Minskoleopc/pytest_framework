04c88d95e79de6df0daa851a4027d7de2cd9b860
unknown <Minskoleopc>
2025-02-21 20:02:57 +0530

    added

--- pytestProject/tests/conftest.py
+++ pytestProject/tests/conftest.py
@@ -1,38 +1,26 @@
-#fixture
+# #fixture
 import pytest
 from selenium import webdriver
 
-@pytest.fixture(scope="function")
-def browser():
-    print("opening the broswer")
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get('https://webdriveruniversity.com/')
-    yield driver
-    driver.quit()
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("opening the broswer")
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     driver.get('https://webdriveruniversity.com/')
+#     yield driver
+#     driver.quit()
 
-@pytest.fixture(scope="class")
-def browser2():
-    print("\n opening browser for test class ....")
-    driver= webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    print("Closing a browser for test class")
-    driver.quit()
+# @pytest.fixture(scope="class")
+# def browser2():
+#     print("\n opening browser for test class ....")
+#     driver= webdriver.Chrome()
+#     driver.maximize_window()
+#     yield driver
+#     print("Closing a browser for test class")
+#     driver.quit()
 
-@pytest.fixture(scope="module")
-def browser3():
-    print("\n opening browser for test class ....")
-    driver= webdriver.Chrome()
-    driver.maximize_window()
-    driver.get('https://www.google.com')
-    yield driver
-    print("Closing a browser for test class")
-    driver.quit()
-
-
-# 1st - file 1 testOne.py / testTwo.py / testThree.py 
-# @pytest.fixture(scope="session")
+# @pytest.fixture(scope="module")
 # def browser3():
 #     print("\n opening browser for test class ....")
 #     driver= webdriver.Chrome()
@@ -41,3 +29,30 @@ def browser3():
 #     yield driver
 #     print("Closing a browser for test class")
 #     driver.quit()
+
+
+# # 1st - file 1 testOne.py / testTwo.py / testThree.py 
+# # @pytest.fixture(scope="session")
+# # def browser3():
+# #     print("\n opening browser for test class ....")
+# #     driver= webdriver.Chrome()
+# #     driver.maximize_window()
+# #     driver.get('https://www.google.com')
+# #     yield driver
+# #     print("Closing a browser for test class")
+# #     driver.quit()
+
+
+@pytest.fixture(params=["chrome","firefox"],scope="function")
+def browser(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "firefox":
+        driver = webdriver.Firefox()
+    yield driver
+    driver.quit()
+
+
+
+    
+
---
+++ pytestProject/tests/test_login.py
@@ -0,0 +1,16 @@
+import pytest 
+
+@pytest.mark.parametrize("username,password",[
+    ("username1","pwd1"),
+    ("username2","pwd2"),
+    ("username3","pwd3"),
+    ("username4","pwd4")
+])
+def test_login(username,password):
+    print(f'{username}{password}')
+    assert username != "" and password != ""
+
+
+def test_open_google(browser):
+    browser.get('https://www.google.com')
+    assert "Google" in browser.title
